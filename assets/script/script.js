058acc5a4eda49d2f0add0668cfa3ee5ed229787
FunnyLookingFish <112431728+FunnyLookingFish@users.noreply.github.com>
2022-10-25 21:07:05 -0700

    recreated js layout

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,30 @@
-console.log("This is loaded. You are doing great. Keep up the good work.");
+console.log("This is loaded. You are doing great. Keep up the good work."); 
+
+const APIkey = ;
+
+const cityInputEl = ;
+const searchButton = ;
+const clearButton = ;
+const previousSearchEl = ;
+
+// getWeather function
+function getWeather(data){};
+
+// Search History
+function searchHistory() {};
+
+// Get coordinates function
+function getCoordinates() {};
+
+// Clear history function
+function clearHistory() {};
+
+// Clear current search function
+function clearCurrent() {};
+
+// Submit city name form submit function
+function cityFormSubmit() {};
+
+// Previous city updated search
+function previousCity() {};
+
